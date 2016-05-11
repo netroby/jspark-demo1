d7c68220ac58aa46a4c8aec74189ad0a93ca0c1a
ZhiFeng Hu <hufeng1987@gmail.com>
2016-05-11 16:52:07 +0800

    Modify dependencies. update thread pool
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,10 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+        int maxThreads = 50;
+        int minThreads = 2;
+        int timeOutMillis = 30000;
+        threadPool(maxThreads, minThreads, timeOutMillis);
         get("/hello", (req, res) -> "Hello world");
     }
 }
